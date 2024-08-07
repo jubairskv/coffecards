339890153657996128e2d2b291b70b5782e0facd
jubairskv <jubairskv@gmail.com>
2024-08-07 22:34:46 +0530

    coffecards

--- src/components/MainConatiner.js
+++ src/components/MainConatiner.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import bg from "../../src/assets/bg-cafe.jpg";
 import svg from "../assets/Star.svg";
 import star from "../assets/Star_fill.svg";
+import vector from "../assets/vector.svg";
 
 const MainContainer = () => {
   const [data, setData] = useState("");
@@ -18,9 +19,7 @@ const MainContainer = () => {
     console.log(json);
   };
 
-  let datas = data[0];
-
-  if (!datas) return "null";
+  if (!data) return null;
 
   return (
     <div>
@@ -33,14 +32,20 @@ const MainContainer = () => {
         <div className=" absolute top-[10%] left-[5%] sm:shrink-0">
           <div className="w-[500px] h-[65rem] xss:w-[250px] xss:h-[160rem] x:w-[300px] x:h-[160rem] xs:w-[420px]  xs:h-[160rem] sm:w-[560px] sm:h-[160rem] md:w-[700px] md:h-[160rem] lg:w-[60rem] lg:h-[160rem] xl:w-[75rem] xl:h-[160rem] 2xl:w-[95rem] 2xl:h-[60rem]  text-center rounded-xl text-white p-20 bg-custom-black">
             <div className="flex flex-col justify-center items-center">
-              <div className="font-sans font-bold text-4xl text-wrap">
-                Our Collections
+              <div className="relative font-sans font-bold text-4xl text-wrap">
+                <img
+                  src={vector}
+                  alt="png"
+                  className="absolute top-0 left-0 z-0 ml-[17rem] mt-[-4rem] text-wrap"
+                />
+                <div className="relative z-10">Our Collections</div>
+                <p className="relative font-sans font-medium text-lg text-wrap text-text-gray pt-5 max-w-[550px] z-10">
+                  Introducing our Coffee Collection, a selection of unique
+                  coffees from different roast types and origins, expertly
+                  roasted in small batches and shipped fresh weekly.
+                </p>
               </div>
-              <p className="font-sans font-medium text-lg text-wrap text-text-gray pt-5  max-w-[550px]">
-                Introducing our Coffee Collection,a selection of unique coffees
-                from different roast types and origins, expertly roasted in
-                small batches and shipped fresh weekly.
-              </p>
+
               <div className="pt-5 font-sans font-medium  text-nowrap">
                 <label className=" p-3 bg-color-gray rounded-lg">
                   All Products
