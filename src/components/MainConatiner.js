5af5fc2bf2a7402bd50d300c4d5bbad407b63ec1
jubairskv <jubairskv@gmail.com>
2024-08-07 18:40:26 +0530

    coffecards

--- src/components/MainConatiner.js
+++ src/components/MainConatiner.js
@@ -26,12 +26,12 @@ const MainContainer = () => {
     <div>
       <div>
         <img
-          className="w-100%  h-96 sm:w-[100%] sm:h-96 lg:w-[100%] lg:h-72 xl:w-[100%] xl:h-60  "
+          className="w-100% h-96 sm:w-[100%] sm:h-96 lg:w-[100%] lg:h-72 xl:w-[100%] xl:h-60  "
           src={bg}
           alt="bg_img"
         />
         <div className=" absolute top-[10%] left-[5%] sm:shrink-0">
-          <div className="w-[500px] h-[45rem] xss:w-[250px] x:w-[300px] xs:w-[420px] sm:w-[560px] md:w-[700px] lg:w-[60rem] xl:w-[75rem] 2xl:w-[95rem]  text-center rounded-xl text-white p-20 bg-custom-black">
+          <div className="w-[500px] h-[65rem] xss:w-[250px] x:w-[300px] xs:w-[420px] sm:w-[560px] md:w-[700px] lg:w-[60rem] xl:w-[75rem] 2xl:w-[95rem]  text-center rounded-xl text-white p-20 bg-custom-black">
             <div className=" flex flex-col justify-center items-center">
               <div className="font-sans font-bold text-4xl text-wrap">
                 Our Collections
@@ -57,7 +57,7 @@ const MainContainer = () => {
                         className="rounded-xl"
                       />
                       <p className="absolute top-0 left-0 text-black bg-yellow-300 pl-2 pr-2 p-1 m-2 font-sans font-semibold text-sm rounded-3xl">
-                        {item.popular ? "Popular" : ""}
+                        {item.popular ? "Popular" : ''}
                       </p>
                     </div>
                     <div className="flex justify-between pt-3">
